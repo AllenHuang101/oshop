3e4291824e0f541f76fd22f37700efcbfcc32d42
allen3_huang <allen3_huang@asus.com>
2018-12-15 09:24:14 +0800

    init commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAwGvxPubV-c6g5vMd5sm6WjOgb7xkKEgU",
+    authDomain: "oshop-cf07b.firebaseapp.com",
+    databaseURL: "https://oshop-cf07b.firebaseio.com",
+    projectId: "oshop-cf07b",
+    storageBucket: "",
+    messagingSenderId: "266275693700"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAwGvxPubV-c6g5vMd5sm6WjOgb7xkKEgU",
+    authDomain: "oshop-cf07b.firebaseapp.com",
+    databaseURL: "https://oshop-cf07b.firebaseio.com",
+    projectId: "oshop-cf07b",
+    storageBucket: "",
+    messagingSenderId: "266275693700"
+  }
 };
 
 /*
